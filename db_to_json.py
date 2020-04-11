4bb6fc4107c41464f88ebb300897ca61ef46c557
Henrique Morimitsu <henriquem87@gmail.com>
2020-04-11 12:44:47 +0800

    Add comment

--- db_to_json.py
+++ db_to_json.py
@@ -4,6 +4,7 @@ from utils import Config, dump_db_as_json
 
 
 def main():
+    """ Simple script to convert a pickle database into a json database. """
     db = pickle.load(open(Config.db_path, 'rb'))
     dump_db_as_json(db)
 
